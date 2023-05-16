668bc023f2edc508eb22cb9340503939f2e159f6
Brian Cheng <bscheng@gmail.com>
2023-05-16 11:03:14 -0400

    updated fig 2b

--- code/Fig2.R
+++ code/Fig2.R
@@ -136,7 +136,7 @@ summary(weight01b)
 
 
 #### SGR analysis and plotting
-sgr<-read.csv(here('data/BT_Morphometrics_SGR_LongForm_Merged_05032023.csv'), stringsAsFactors = T)
+sgr<-read.csv(here('data/BT_Morphometrics_SGR_LongForm_Merged_05102023.csv'), stringsAsFactors = T)
 str(sgr)
 which(sgr$SGR < 0)
 sgr <- sgr %>%
@@ -147,6 +147,8 @@ sgr <- sgr %>%
   rename ("time_step" = "condition3") %>%
   filter (condition1 !="SGR_6") %>%
   filter (SGR > -0.2) #remove mass = 0 fish
+
+ggplot(sgr, aes(x=time_step, y=Days_in_Exp))+geom_point()
 
 #### summarize means
 sgr_means <-tapply(sgr$SGR,list(sgr$temp,sgr$time_step),mean)
@@ -160,4 +162,69 @@ sgr_means <-sgr_means %>%
   pivot_longer(cols = starts_with("T"),
                names_to = "time_step",
                names_prefix = "T",
-               values_to = "mean_weight")
+               values_to = "mean_sgr")
+
+sgr_sem<-data.frame(sgr_sem)
+sgr_sem$C_temp<-row.names(sgr_sem)
+sgr_sem <-sgr_sem %>%
+  pivot_longer(cols = starts_with("T"),
+               names_to = "time_step",
+               names_prefix = "T",
+               values_to = "se_sgr")
+
+sgr_mean_se<- left_join(sgr_means, sgr_sem, by = c("time_step", "C_temp"))
+sgr_mean_se<- sgr_mean_se %>%
+  mutate (time_step = factor(time_step))%>%
+  mutate (Days = case_when (time_step == "1"~ 0,
+                            time_step == "2"~ 20,
+                            time_step == "3"~ 62,
+                            time_step == "4"~ 98,
+                            time_step == "5"~ 133,
+                            time_step == "6"~ 161,
+                            time_step == "8"~ 244))
+
+#### create MS plots, 2B second
+A_col <- "#9933FF"
+B_col <- "#FF6666"
+str(sgr_mean_se)
+plot2<-ggplot(sgr_mean_se, aes(x=Days, y=mean_sgr, color = C_temp,group = C_temp))+
+  geom_point(size = 3.5, position = position_dodge(width = 0.2))+
+  geom_errorbar(data=sgr_mean_se, aes(ymin=mean_sgr-se_sgr, ymax = mean_sgr+se_sgr),
+                width=0, size = 1, position = position_dodge(width = 0.2))+
+  geom_line(aes(group = C_temp), position = position_dodge(width = 0.2))+
+  labs(y=expression(paste("Specific Growth Rate (mean \U00B1SEM)")), x="Days in experiment")+
+  theme_bw()+theme(legend.title=element_text(size=16),legend.text=element_text(size=14),
+                   axis.text.y=element_text(size=16),axis.title.y=element_text(size=18, vjust=1.2),
+                   axis.text.x=element_text(size=16),axis.title.x=element_text(size=18),
+                   panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                   legend.position = "none")+
+  scale_fill_manual(values  = c(A_col, B_col)) +
+  scale_color_manual(values = c("15" = A_col, "20" = B_col))+
+  coord_cartesian(xlim = c(0,250), ylim = c(0,4))
+plot2
+ppi=300
+png('figures/sgr_time_series.png', width=9*ppi, height=6*ppi, res=ppi)
+plot2
+dev.off()
+
+#### SGR analysis
+str(sgr)
+
+sgr01<-lmer(SGR ~ temp * time_step + (1|TAGNUMBER), data = sgr)
+sgr02<-lmer(SGR ~ temp * time_step + (1|TAGNUMBER) + (1|tank), data = sgr)
+summary(sgr01)
+summary(sgr02)
+
+plot(sgr01)
+sgr$residuals<-residuals(sgr01)
+ggplot(sgr,aes(x=time_step, y= residuals))+geom_boxplot()
+ggplot(sgr,aes(x=temp, y= residuals))+geom_boxplot()+facet_grid(.~time_step)
+
+anova(sgr01, ddf = "Kenward-Roger")
+anova(sgr02, ddf = "Kenward-Roger")
+
+emm_sgr = emmeans (sgr01, ~ temp | time_step)
+contrast(emm_sgr, contrast = TRUE)
+pairs(emm_sgr)
+pairs(emm_sgr, by = "temp")
+plot(emm_sgr)
